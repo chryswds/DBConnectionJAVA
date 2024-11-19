f76ed95f61002fbb574b049a21335fb687fff952
chryswds <dybas.chrys@gmail.com>
2024-11-19 09:55:54 +0000

    method returning all patient information getAllData() method

--- src/main/java/org/example/DatabaseReader.java
+++ src/main/java/org/example/DatabaseReader.java
@@ -33,16 +33,25 @@ public class DatabaseReader extends DB_Connection {
             // Now that the connection is established
             // We begin creating the logic for reading the data from the db
             // There will be multiple records, we would like to iterate through all the records
-
             ResultSet results = stmt.executeQuery(String.format("SELECT * FROM %s;", TABLE));
             // Create a check for results and create a while loop to iterate through them
-            while()
-
-
-
+            // For every result we have the data will look like this
+            // Name, BirthData, BloodType, ID
+            // As long as we have more rows to go through we will keep iterating
+            while(results.next()){
+                String name = results.getString("name"); // Patient name
+                String birthdate = results.getString("birth_date"); // Patient BD
+                String bloodtype = results.getString("blood_type"); // Patient BT
+                int id = results.getInt("id"); // Patient ID
+                // Format the output from the query and insert into a collection
+                Patient patient = new Patient(name, birthdate, bloodtype, id);
+                patients.add(patient);
+            }
         }catch(Exception e){
             e.printStackTrace();
         }
+        //returning the arraylist data
+        return patients;
 
 
     }
